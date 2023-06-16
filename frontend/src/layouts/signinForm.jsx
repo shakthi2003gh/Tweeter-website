4ec42260999f3b656440d7ae1cb756cdcda52b62
shakthi <shakthikumar123456789@gmail.com>
2023-06-16 23:36:40 +0530

    Create signin form

---
+++ frontend/src/layouts/signinForm.jsx
@@ -0,0 +1,68 @@
+import { useMemo, useState } from "react";
+import { Link } from "react-router-dom";
+import Joi from "joi";
+import InputGroup from "../components/inputGroup";
+
+function SigninForm() {
+  const [loading, setLoading] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const schema = {
+    email: Joi.string().email({ tlds: false }).required().min(5).max(50),
+    password: Joi.string().required().min(5).max(50),
+  };
+
+  const isDisabled = useMemo(() => {
+    const { error } = Joi.object(schema).validate({ email, password });
+
+    return !!error?.details[0].message;
+  }, [email, password, schema]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <InputGroup
+        type="email"
+        label="Email"
+        value={email}
+        onChange={setEmail}
+        error={emailError}
+        setError={setEmailError}
+        schema={schema.email}
+      />
+
+      <InputGroup
+        type="Password"
+        label="Password"
+        value={password}
+        onChange={setPassword}
+        error={passwordError}
+        setError={setPasswordError}
+        schema={schema.password}
+      />
+
+      <button
+        type="submit"
+        className="btn btn-primary mt-2"
+        style={{ float: "right" }}
+        disabled={isDisabled || loading}
+      >
+        Submit
+      </button>
+
+      <Link to={"/auth?method=signup"} className="btn btn-secondary mt-2">
+        Signup
+      </Link>
+    </form>
+  );
+}
+
+export default SigninForm;
