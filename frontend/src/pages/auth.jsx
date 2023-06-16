423c5c8fdba7c3dfb288cc012a213fa18e49d40c
shakthi <shakthikumar123456789@gmail.com>
2023-06-16 23:53:13 +0530

    Create auth page

--- frontend/src/pages/auth.jsx
+++ frontend/src/pages/auth.jsx
@@ -1,5 +1,32 @@
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import SignupForm from "../layouts/signupForm";
+import SigninForm from "../layouts/signinForm";
+import "react-toastify/dist/ReactToastify.css";
+
 function Auth() {
-  return <div className="auth">Auth</div>;
+  let [searchParams, setSearchParams] = useSearchParams();
+  const methods = ["signin", "signup"];
+  const method = searchParams.get("method");
+
+  useEffect(() => {
+    if (!methods.includes(method)) setSearchParams({ method: "signin" });
+  }, [method]);
+
+  return (
+    <div className="auth flex-center vh-100">
+      <div
+        className="container align-items-center w-100 px-3"
+        style={{ maxWidth: "390px" }}
+      >
+        <h1 className="text-center mb-5 fw-bolder text-primary text-capitalize">
+          {method}
+        </h1>
+
+        {method === "signup" ? <SignupForm /> : <SigninForm />}
+      </div>
+    </div>
+  );
 }
 
 export default Auth;
