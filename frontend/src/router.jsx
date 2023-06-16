7ec290bd86d07781a7f77086d98c86f84e0e6d18
shakthi <shakthikumar123456789@gmail.com>
2023-06-16 23:54:50 +0530

    Update router file

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,8 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import App from "./App";
 import Auth from "./pages/auth";
+import { verifyUser } from "./services/http";
 
 function Router() {
+  const navigate = useNavigate();
+
+  const checkUser = () => {
+    const token = localStorage.getItem("x-tweeter-auth");
+    if (!token) return navigate("/auth");
+
+    verifyUser(token).catch(() => navigate("/auth"));
+  };
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
   return (
     <Routes>
       <Route path="/auth" element={<Auth />} />
