1fabcd697ad4f40dd0518b25a6d0e2833ce69df2
shakthi <shakthikumar123456789@gmail.com>
2023-06-16 23:48:06 +0530

    Setup toastify

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router";
+import { ToastContainer } from "react-toastify";
 import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Router />
+      <ToastContainer className="p-2" />
     </BrowserRouter>
   </React.StrictMode>
 );
