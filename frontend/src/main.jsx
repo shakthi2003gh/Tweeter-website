5691772c0caadb776abf159c5f6aac64ec231d91
shakthi <shakthikumar123456789@gmail.com>
2023-06-18 00:35:16 +0530

    Setup redux store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import Router from "./router";
+import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
+import Router from "./router";
+import store from "./store/index";
 import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Router />
+      <Provider store={store}>
+        <Router />
+      </Provider>
       <ToastContainer className="p-2" />
     </BrowserRouter>
   </React.StrictMode>
