004dd2fa8f694b00a8cf668f7e08993d57fac5d5
shakthi <shakthikumar123456789@gmail.com>
2023-06-21 12:04:44 +0530

    Create home page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,9 +1,19 @@
+import { useSelector } from "react-redux";
 import PostComposer from "../layouts/postComposer";
+import Post from "../layouts/post";
 
 function Home() {
+  const posts = useSelector((state) => state.posts.home);
+
   return (
     <div className="home">
       <PostComposer />
+
+      <div className="posts">
+        {posts.map((post) => (
+          <Post key={post._id} post={post} />
+        ))}
+      </div>
     </div>
   );
 }
